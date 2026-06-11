66a6ab295fcc5555372caaad5cddf94a5f9d0d67
jh145666 <jh145666@foxmail.com>
2026-06-11 18:25:26 +0800

    Update AppletDetailActivity.java

--- app/src/main/java/at/fhooe/usmile/gpjshell/AppletDetailActivity.java
+++ app/src/main/java/at/fhooe/usmile/gpjshell/AppletDetailActivity.java
@@ -18,7 +18,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
